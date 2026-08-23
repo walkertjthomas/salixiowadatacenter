bd3b9628c3a3d8bed16f783eecdf7e99fe3f6c69
TJ Walker <tjwalkerunomaha@gmail.com>
2026-08-22 22:54:20 -0700

    Update nav.js

--- assets/nav.js
+++ assets/nav.js
@@ -2,12 +2,12 @@ class SiteNav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav class="top-nav">
-                <div class="nav-brand">My Project</div>
+                <div class="nav-brand">Salix Info</div>
                 <ul class="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#features">Information</a></li>
-                    <li><a href="#about">News</a></li>
-                    <li><a href="#contact">Community</a></li>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="information.html">Information</a></li>
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="community.html">Community</a></li>
                 </ul>
             </nav>
         `;
